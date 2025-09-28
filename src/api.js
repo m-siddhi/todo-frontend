6662d59c58382b02882555873f98b8e208a74d17
Siddhi <manjrekarsiddhi72@gmail.com>
2025-09-29 02:11:53 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
-
 const api = axios.create({
-  baseURL: API_URL, // remove extra /api
+  baseURL: process.env.REACT_APP_API_URL + "/api", // matches your backend
 });
 
 export default api;
