23a2e429f573034587697b0c81dc975fa532690a
Siddhi <manjrekarsiddhi72@gmail.com>
2025-09-29 00:28:16 +0530

    corrected api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-let API_URL = "http://localhost:5000/api";
-if (process.env.REACT_APP_API_URL) {
-  API_URL = process.env.REACT_APP_API_URL;
-}
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
